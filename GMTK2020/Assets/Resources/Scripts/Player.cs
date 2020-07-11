a802493192a25f222f78a3145c81f4c2ceb2e077
cortejon <cortejon@cs.washington.edu>
2020-07-10 22:15:12 -0700

    initial setup

---
+++ GMTK2020/Assets/Resources/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField] private float speed;
+    [SerializeField] private Vector2 JUMP_FORCE;  // readonly
+    [SerializeField] private Vector2 COUNTER_JUMP_FORCE;  // readonly
+
+    private bool facingRight;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
