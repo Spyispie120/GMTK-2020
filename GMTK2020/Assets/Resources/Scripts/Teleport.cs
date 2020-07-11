c313023281f34b8d4acb5b73d1320bebb30b0ea2
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-07-10 23:08:12 -0700

    Added teleport(not test)

---
+++ GMTK2020/Assets/Resources/Scripts/Teleport.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : Ability
+{
+    private Player player;
+
+    [SerializeField]
+    private Vector2 teleportDistance;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void Activate()
+    {
+        Vector3 pos = player.transform.position;
+        player.transform.position = new Vector3(pos.x + teleportDistance.x, pos.y + teleportDistance.y, pos.z);
+    }
+
+}
