4d8555cb0e85c2192732995e005fba13af18108b
cortejon <cortejon@cs.washington.edu>
2020-07-11 21:58:42 -0700

    player flashes when teleporter is online again

---
+++ GMTK2020/Assets/Resources/Scripts/Cooldown.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cooldown : MonoBehaviour
+{
+    private Teleport player;
+    private Image img;
+    public float heightScale = 1.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<Teleport>();
+        img = GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float viewportHeight = Camera.main.pixelRect.height / heightScale;
+        gameObject.transform.position = Camera.main.WorldToScreenPoint(player.transform.position) +
+                                        new Vector3(0, viewportHeight, 0);
+        img.fillAmount = (Mathf.Abs(player.GetCooldownRatio()) < 0.001f) ? 0 : 1.0f - player.GetCooldownRatio();
+    }
+}
--- GMTK2020/Assets/Resources/Scripts/Teleport.cs
+++ GMTK2020/Assets/Resources/Scripts/Teleport.cs
@@ -9,6 +9,12 @@ public class Teleport : Ability
     private Vector2 teleportDistance;
     private bool canUse;
 
+    private SpriteRenderer sr;
+    private Material blueMat;
+
+    [SerializeField] private float COOLDOWN = 0.3f;
+    [SerializeField] private float cdTimer;
+
     [SerializeField]
     private GameObject ghostTrail;
 
@@ -16,10 +22,43 @@ public class Teleport : Ability
     {
         base.Start();
         canUse = true;
+        cdTimer = 0;
+        sr = GetComponent<SpriteRenderer>();
+        blueMat = Resources.Load("Materials/BlueFlash", typeof(Material)) as Material;
+        //Debug.Log(blueMat);
+        //sr.material = blueMat;
+        //StartCoroutine("Flash");
     }
-    
+
+    private void Update()
+    {
+        if (cdTimer > 0)
+        {
+            cdTimer -= Time.deltaTime;
+            if (cdTimer <= 0)
+            {
+                StartCoroutine(Flash());
+            }
+        }
+    }
+
+    IEnumerator Flash()
+    {
+        Material m = sr.material;
+        sr.material = blueMat;
+        yield return new WaitForSeconds(0.1f);
+        sr.material = m;
+    }
+
+    public float GetCooldownRatio()
+    {
+        return Mathf.Max(0, cdTimer) / COOLDOWN;
+    }
+
     public override void Activate()
     {
+        if (cdTimer > 0) return;
+        cdTimer = COOLDOWN;
         //if (!canUse) return;
         player.anim.SetTrigger("Teleport");
         CreateGhostTrail();
