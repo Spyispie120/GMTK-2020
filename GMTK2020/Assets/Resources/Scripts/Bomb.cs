90a18dd3560a6f9f5bbb18773e1c89af7c38baf0
cortejon <cortejon@cs.washington.edu>
2020-07-12 08:12:08 -0700

    magnetism

--- GMTK2020/Assets/Resources/Scripts/Bomb.cs
+++ GMTK2020/Assets/Resources/Scripts/Bomb.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
+    private GameObject particles;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        particles = Resources.Load("Particles/Explosion2") as GameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Player>())
+        {
+            StartCoroutine(PlayerDeath());
+        }
+    }
+
+    IEnumerator PlayerDeath()
+    {
+        Instantiate(particles, transform.position, Quaternion.identity);
+        yield return new WaitForSeconds(0.2f);
+        GlobalValues.Instance.GetPlayer().Die();
     }
 }
