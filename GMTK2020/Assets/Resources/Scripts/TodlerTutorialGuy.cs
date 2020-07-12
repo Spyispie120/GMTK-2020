a02e8998e3cdd3ca67cb2780bc5e673fa5196a6a
Nguyen Duong <ducnguyenduchao@gmail.com>
2020-07-12 10:34:58 -0700

    Change quote in explosive and music in laser level

--- GMTK2020/Assets/Resources/Scripts/TodlerTutorialGuy.cs
+++ GMTK2020/Assets/Resources/Scripts/TodlerTutorialGuy.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class TodlerTutorialGuy : MonoBehaviour
 {
@@ -17,6 +18,10 @@ public class TodlerTutorialGuy : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
+            if(SceneManager.GetActiveScene().name == "Laser")
+            {
+                Jukebox.Instance.PlaySound("gametime");
+            }
             Player player = collision.gameObject.GetComponent<Player>();
             player.abilityActivation.EnableAbility(abilities.ToString());
         }
