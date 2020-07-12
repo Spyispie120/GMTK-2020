ca61f586fcaf5244058dd4e520c072a2d521f411
BoyNamedHsu <44332341+BoyNamedHsu@users.noreply.github.com>
2020-07-12 03:07:21 -0700

    Importing text mesh pro

---
+++ GMTK2020/Assets/Resources/Scripts/DialogueTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    [SerializeField] private string[] dialogue;
+    private int dialoguePointer;
+    public TextMeshProUGUI text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            text.text = dialogue[dialoguePointer];
+            dialoguePointer++;
+            if (dialoguePointer > dialogue.Length - 1)
+            {
+                dialoguePointer = 0;
+            }
+        }
+    }
+}
